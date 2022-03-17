1d76bc979d0b7e13e1bc55a1552c07478c8b1460
0blto <100999960+0blto@users.noreply.github.com>
2022-03-17 22:17:30 +0300

    Delete lab5renew directory

--- lab5renew/src/util/OutputUtil.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class OutputUtil {
-    public static void outputManager(String s) {
-        System.out.print(s);
-    }
-
-    public static void outputManagerLiner(String s) {System.out.println(s);}
-}
